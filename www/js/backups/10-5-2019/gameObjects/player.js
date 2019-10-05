af5e0247f9a2cc444fd73bd14ffc5310e937fd53
Thundros <aquariaxi@protonmail.com>
2019-10-05 14:44:31 -0500

    Made some changes

---
+++ www/js/backups/10-5-2019/gameObjects/player.js
@@ -0,0 +1,75 @@
+export default class Player {
+
+    constructor (scene, x, y)
+    {
+        // Set player
+        this.sprite = scene.physics.add.sprite(x, y, "player");
+        this.sprite.setDrag(100, 0).setMaxVelocity(250, 600).setDrag(100, 20);
+      
+        this.keys = scene.input.keyboard.createCursorKeys();
+    }
+
+    update (time, delta)
+    {   
+        if(this.dead)
+        {
+            return;
+        }
+
+        // Set up variables
+        const [ keys, sprite ] = [this.keys, this.sprite];
+        let speed = 1000;
+        let jumpHeight = 490;
+
+        // Move left or right
+        if(keys.left.isDown) 
+        {
+            sprite.body.setAccelerationX(-speed).setDrag(230, 20);
+            sprite.setFlipX(true);
+        }
+        else if(keys.right.isDown) 
+        {
+            sprite.body.setAccelerationX(speed).setDrag(230, 20);
+            sprite.setFlipX(false);
+        }else{
+            sprite.setAccelerationX(0).setDrag(600, 20);
+        }
+
+        // Jump mechanics
+        if(keys.up.isDown && sprite.body.onFloor()) 
+        {
+            sprite.body.setVelocityY(-jumpHeight);
+        }
+
+        if(sprite.y > levelHandler.blockLayer.height + sprite.height)
+        {
+            this.kill();
+        }
+    }
+
+    onCollide (object, name)
+    {
+        switch(name)
+        {
+            case "lava" :
+                this.kill();
+                break;
+
+            case "door" :
+                if(this.keys.down.isDown && this.sprite.body.onFloor())
+                {
+                    this.enteredDoor = true;
+                    this.touchedObject = object;
+
+                    this.sprite.body.stop();
+                }
+                break;
+        }
+    }
+
+    kill ()
+    {
+        this.sprite.body.stop();
+        this.dead = true;
+    }
+}
---
+++ www/js/backups/10-5-2019/scenes/playScene.js
@@ -0,0 +1,125 @@
+import Player from "../gameObjects/player.js";
+import game from "../game.js";
+
+export default class PlayScene extends Phaser.Scene {
+
+    constructor ()
+    {
+        super('playScene');
+    }
+
+    preload ()
+    {
+        // This is probably not the way this was intended to use but okay.
+        this.load.tilemapTiledJSON(levelHandler.levelName, "assets/tilemaps/" + levelHandler.levelName + ".json");
+
+        this.load.spritesheet("otherTiles-extruded", "assets/tilesets/otherTiles-extruded.png", 
+        { 
+            frameWidth: 32, 
+            frameHeight: 32 
+        });
+
+        this.load.image("player", 'assets/spritesheets/player.png');
+    }
+
+    create ()
+    {
+        // Setup up the tilemap level, get the tile image, create a dynamic layer and set collsion
+        levelHandler.level = this.make.tilemap({ key: levelHandler.levelName });
+        levelHandler.otherTiles = levelHandler.level.addTilesetImage("otherTiles-extruded", "otherTiles-extruded");
+        levelHandler.blockLayer = levelHandler.level.createDynamicLayer("World", [levelHandler.otherTiles], 0, 0);
+        levelHandler.blockLayer.setCollisionByExclusion([-1, TILES.LAVA, TILES.DOORUP, TILES.DOORDOWN]);
+
+       
+        let spawnPoint = {};
+        switch(levelHandler.travelType)
+        {
+            case "spawnPoint" :
+                    // Find player spawn point and place him there
+                    spawnPoint = levelHandler.level.findObject("Objects", obj => obj.name === "Player Spawn Point");
+                break;
+
+            case "door" :
+                    // Find corresponding door and place him there.
+                    levelHandler.level.findObject("Objects", obj => 
+                    {    
+                        // We found a match
+                        if(obj.type === "door" && obj.name.replace("Door", "").replace("door", "") === levelHandler.doorSymbol)
+                        {
+                            spawnPoint.x = obj.x + 16;
+                            spawnPoint.y = obj.y + 32 + (obj.height - this.player.sprite.height);
+                        }
+                    });
+                break;
+        }
+
+        // Ugh this will get in the way when bringing specific data to be saved but not saved yet...
+        this.player = new Player(this, spawnPoint.x, spawnPoint.y);
+
+        // Set up collision with the player for the walls and tiles
+        this.player.sprite.body.setCollideWorldBounds(true);
+        this.physics.world.setBounds(0, 0, levelHandler.level.widthInPixels, levelHandler.level.heightInPixels, true, true, true, false);
+        this.physics.add.collider(this.player.sprite, levelHandler.blockLayer);
+
+        // Have the camera start following the player and set the camera's bounds
+        this.cameras.main.startFollow(this.player.sprite);
+        this.cameras.main.setBounds(0, 0, levelHandler.level.widthInPixels, levelHandler.level.heightInPixels);
+
+        // Set collision
+        // I should probably start making es6 classes for tiles
+        levelHandler.blockLayer.setTileIndexCallback(TILES.LAVA, function(objectA, objectB)
+        {
+            this.player.onCollide.apply(this.player, [objectB, "lava"]);
+        }, this);
+
+        levelHandler.level.findObject("Objects", obj => 
+        {    
+            if(obj.type === "door")
+            {
+                var object = this.physics.add.sprite(obj.x, obj.y, "door").setOrigin(0, 0).setDepth(-1);
+                object.body.moves = false;
+
+                object.setVisible(false);
+
+                object.obj = obj;
+
+                this.physics.add.overlap(this.player.sprite, object, function(objectA, objectB)
+                {
+                    this.player.onCollide.apply(this.player, [objectB, "door"]);
+                }, 
+                null, this);
+            }
+        });
+
+        this.doThisDoor = false;
+        this.doThisDeath = false;
+    }
+
+    update ()
+    {
+        if(this.doThisDeath || this.doThisDoor)
+        {
+            return;
+        }
+
+        this.player.update();
+
+        if(this.player.dead)
+        {
+            this.doThisDeath = true;
+
+            game.gameOver(this);
+        }
+        if(this.player.enteredDoor)
+        {
+            this.doThisDoor = true;
+            
+            game.onDoor(this);
+        }
+    }
+
+    render ()
+    {
+        
+    }
+}
